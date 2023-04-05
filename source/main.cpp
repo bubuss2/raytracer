e0997eb8b7b0ba0ac163912539802a72c0c6bd6e
BuBuSS <bubus258@gmail.com>
2023-04-05 21:38:52 +0200

    change main to use new classes and calculate many surfaces

--- source/main.cpp
+++ source/main.cpp
@@ -1,32 +1,20 @@
 #include "color.hpp"
-#include "ray.hpp"
-#include "vector3.hpp"
+#include "common.hpp"
+#include "sphere.hpp"
+#include "surfaceList.hpp"
 
 #include <iostream>
 
-double hit_sphere(const Vector3 &center, double radius, const Ray &r)
+Vector3 ray_color(const Ray &r, const Surface &world)
 {
-    Vector3 oc = r.get_origin() - center;
-    auto a = r.get_direction().squared_sum();
-    auto half_b = dot(oc, r.get_direction());
-    auto c = oc.squared_sum() - radius * radius;
-    auto discriminant = half_b * half_b - a * c;
-
-    return (discriminant < 0) ? -1.0 : (-half_b - sqrt(discriminant)) / a;
-}
-
-Vector3 ray_color(const Ray &r)
-{
-    auto t = hit_sphere(Vector3(0, 0, -1), 0.5, r);
-
-    if (t > 0.0)
+    HitRecord rec;
+    if (world.hit(r, 0, infinity, rec))
     {
-        Vector3 N = unit_vector(r.at(t) - Vector3(0, 0, -1));
-        return 0.5 * Vector3(N.x() + 1, N.y() + 1, N.z() + 1);
+        return 0.5 * (rec.normal + Vector3(1, 1, 1));
     }
 
     Vector3 unit_direction = unit_vector(r.get_direction());
-    t = 0.5 * (unit_direction.y() + 1.0);
+    auto t = 0.5 * (unit_direction.y() + 1.0);
     return (1.0 - t) * Vector3(1.0, 1.0, 1.0) + t * Vector3(0.5, 0.7, 1.0);
 }
 
@@ -37,6 +25,11 @@ int main()
     const auto aspect_ratio = 16.0 / 9.0;
     const int image_width = 400;
     const int image_height = static_cast<int>(image_width / aspect_ratio);
+
+    // World
+    SurfaceList world;
+    world.add(std::make_shared<Sphere>(Vector3(0, 0, -1), 0.5));
+    world.add(std::make_shared<Sphere>(Vector3(0, -100.5, -1), 100));
 
     // Camera
 
@@ -61,7 +54,7 @@ int main()
             auto u = double(i) / (image_width - 1);
             auto v = double(j) / (image_height - 1);
             Ray r(origin, lower_left_corner + u * horizontal + v * vertical - origin);
-            Vector3 pixel_color = ray_color(r);
+            Vector3 pixel_color = ray_color(r, world);
             write_color(std::cout, pixel_color);
         }
     }
