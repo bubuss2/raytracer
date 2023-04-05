08c2f0ba4b1d196994c4face2ac455ee213c29d0
BuBuSS <bubus258@gmail.com>
2023-04-05 21:32:28 +0200

    create surfaceList class

---
+++ source/surfaceList.hpp
@@ -0,0 +1,56 @@
+#ifndef SURFACEList_HPP
+#define SURFACEList_HPP
+
+#include "surface.hpp"
+
+#include <memory>
+#include <vector>
+
+using std::make_shared;
+using std::shared_ptr;
+
+class SurfaceList : public Surface
+{
+  public:
+    SurfaceList()
+    {
+    }
+    SurfaceList(shared_ptr<Surface> object)
+    {
+        add(object);
+    }
+
+    void clear()
+    {
+        objects.clear();
+    }
+
+    void add(shared_ptr<Surface> object)
+    {
+        objects.push_back(object);
+    }
+
+    virtual bool hit(const Ray &r, double t_min, double t_max, HitRecord &rec) const
+    {
+        HitRecord temp_rec;
+        bool hit_anything = false;
+        auto closest_so_far = t_max;
+
+        for (const auto &object : objects)
+        {
+            if (object->hit(r, t_min, closest_so_far, temp_rec))
+            {
+                hit_anything = true;
+                closest_so_far = temp_rec.t;
+                rec = temp_rec;
+            }
+        }
+
+        return hit_anything;
+    }
+
+  public:
+    std::vector<shared_ptr<Surface>> objects;
+};
+
+#endif
