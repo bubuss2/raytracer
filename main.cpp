a118ff9f9c05624d7bfdc92115a9f00012d663f4
BuBuSS <bubus258@gmail.com>
2023-04-05 18:44:36 +0200

    change main to use vector3 and write_color helper function

--- main.cpp
+++ main.cpp
@@ -1,24 +1,26 @@
+#include "color.hpp"
+#include "vector3.hpp"
+
 #include <iostream>
 
 int main()
 {
+    // Image
     const int image_width = 512;
     const int image_height = 512;
+
+    // Render
     std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
 
-    for (int j = image_height - 1; j >= 0; --j)
+    for (std::size_t j = image_height - 1; j >= 0; --j)
     {
-        for (int i = 0; i < image_width; ++i)
+        std::cerr << "\rScanlines remaining: " << j << ' ' << std::flush;
+        for (std::size_t i = 0; i < image_width; ++i)
         {
-            auto r = double(i) / (image_width - 1);
-            auto g = double(j) / (image_height - 1);
-            auto b = 0.25;
-
-            int ir = static_cast<int>(255.999 * r);
-            int ig = static_cast<int>(255.999 * g);
-            int ib = static_cast<int>(255.999 * b);
-
-            std::cout << ir << ' ' << ig << ' ' << ib << '\n';
+            Vector3 pixel_color(double(i) / (image_width - 1), double(j) / (image_height - 1), 0.25);
+            write_color(std::cout, pixel_color);
         }
     }
+
+    std::cerr << "\nDone.\n";
 }
