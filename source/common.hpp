1826f6c71930138e3408225108e4a6356280c799
BuBuSS <bubus258@gmail.com>
2023-04-06 17:29:32 +0200

    create random_double function in common.hpp

--- source/common.hpp
+++ source/common.hpp
@@ -4,6 +4,7 @@
 #include <cmath>
 #include <limits>
 #include <memory>
+#include <random>
 
 // Constants
 
@@ -15,6 +16,13 @@ const double pi = 3.1415926535897932385;
 inline double degrees_to_radians(double degrees)
 {
     return degrees * pi / 180.0;
+}
+
+inline double random_double(double start = 0.0, double end = 1.0)
+{
+    static std::uniform_real_distribution<double> distribution(start, end);
+    static std::mt19937 generator;
+    return distribution(generator);
 }
 
 // Common Headers
