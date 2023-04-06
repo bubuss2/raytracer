fa8ac6c7f69692c8420fae1f217aaf143c04541c
BuBuSS <bubus258@gmail.com>
2023-04-06 20:00:48 +0200

    change scatter formula in dielectric material

--- source/main.cpp
+++ source/main.cpp
@@ -49,9 +49,9 @@ int main()
     // World
     SurfaceList world;
     auto material_ground = std::make_shared<Lambertian>(Color(0.8, 0.8, 0.0));
-    auto material_center = std::make_shared<Lambertian>(Color(0.7, 0.3, 0.3));
-    auto material_left = std::make_shared<Metal>(Color(0.8, 0.8, 0.8), 0.3);
-    auto material_right = std::make_shared<Metal>(Color(0.8, 0.6, 0.2), 1.0);
+    auto material_center = std::make_shared<Lambertian>(Color(0.1, 0.2, 0.5));
+    auto material_left = std::make_shared<Dielectric>(1.5);
+    auto material_right = std::make_shared<Metal>(Color(0.8, 0.6, 0.2), 0.0);
 
     world.add(std::make_shared<Sphere>(Point(0.0, -100.5, -1.0), 100.0, material_ground));
     world.add(std::make_shared<Sphere>(Point(0.0, 0.0, -1.0), 0.5, material_center));
