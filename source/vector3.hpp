dc032313310cc093b69411ccdb6c6d99de343e5b
BuBuSS <bubus258@gmail.com>
2023-04-06 17:25:27 +0200

    change vector3.hpp to use coordinates getters instead of values array

--- source/vector3.hpp
+++ source/vector3.hpp
@@ -35,7 +35,7 @@ class Vector3
 
     Vector3 operator-() const
     {
-        return Vector3(-_values[0], -_values[1], -_values[2]);
+        return Vector3(-x(), -y(), -z());
     }
 
     double operator[](int i) const
@@ -78,43 +78,33 @@ class Vector3
 
     double squared_sum() const
     {
-        return std::pow(_values[0], 2) + std::pow(_values[1], 2) + std::pow(_values[2], 2);
+        return std::pow(x(), 2) + std::pow(y(), 2) + std::pow(z(), 2);
     }
-
-    friend std::ostream &operator<<(std::ostream &out, const Vector3 &v);
-    friend Vector3 operator+(const Vector3 &u, const Vector3 &v);
-    friend Vector3 operator-(const Vector3 &u, const Vector3 &v);
-    friend Vector3 operator*(const Vector3 &u, const Vector3 &v);
-    friend Vector3 operator*(double t, const Vector3 &v);
-    friend Vector3 operator*(const Vector3 &v, double t);
-    friend Vector3 operator/(Vector3 v, double t);
-    friend double dot(const Vector3 &u, const Vector3 &v);
-    friend Vector3 cross(const Vector3 &u, const Vector3 &v);
 };
 
 inline std::ostream &operator<<(std::ostream &out, const Vector3 &v)
 {
-    return out << v._values[0] << ' ' << v._values[1] << ' ' << v._values[2];
+    return out << v.x() << ' ' << v.y() << ' ' << v.z();
 }
 
 inline Vector3 operator+(const Vector3 &u, const Vector3 &v)
 {
-    return Vector3(u._values[0] + v._values[0], u._values[1] + v._values[1], u._values[2] + v._values[2]);
+    return Vector3(u.x() + v.x(), u.y() + v.y(), u.z() + v.z());
 }
 
 inline Vector3 operator-(const Vector3 &u, const Vector3 &v)
 {
-    return Vector3(u._values[0] - v._values[0], u._values[1] - v._values[1], u._values[2] - v._values[2]);
+    return Vector3(u.x() - v.x(), u.y() - v.y(), u.z() - v.z());
 }
 
 inline Vector3 operator*(const Vector3 &u, const Vector3 &v)
 {
-    return Vector3(u._values[0] * v._values[0], u._values[1] * v._values[1], u._values[2] * v._values[2]);
+    return Vector3(u.x() * v.x(), u.y() * v.y(), u.z() * v.z());
 }
 
 inline Vector3 operator*(double t, const Vector3 &v)
 {
-    return Vector3(t * v._values[0], t * v._values[1], t * v._values[2]);
+    return Vector3(t * v.x(), t * v.y(), t * v.z());
 }
 
 inline Vector3 operator*(const Vector3 &v, double t)
@@ -129,14 +119,12 @@ inline Vector3 operator/(Vector3 v, double t)
 
 inline double dot(const Vector3 &u, const Vector3 &v)
 {
-    return u._values[0] * v._values[0] + u._values[1] * v._values[1] + u._values[2] * v._values[2];
+    return u.x() * v.x() + u.y() * v.y() + u.z() * v.z();
 }
 
 inline Vector3 cross(const Vector3 &u, const Vector3 &v)
 {
-    return Vector3(u._values[1] * v._values[2] - u._values[2] * v._values[1],
-                   u._values[2] * v._values[0] - u._values[0] * v._values[2],
-                   u._values[0] * v._values[1] - u._values[1] * v._values[0]);
+    return Vector3(u.y() * v.z() - u.z() * v.y(), u.z() * v.x() - u.x() * v.z(), u.x() * v.y() - u.y() * v.x());
 }
 
 inline Vector3 unit_vector(Vector3 v)
